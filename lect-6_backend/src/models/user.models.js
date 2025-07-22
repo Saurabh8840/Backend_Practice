ae33b4ff542399934a8d76a4adf8ae018556017e
Saurabh8840 <saurabhofficial8840@gmail.com>
2025-07-22 14:59:27 +0530

    added user model

--- lect-6_backend/src/models/user.models.js
+++ lect-6_backend/src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema=new mongoose.Schema({
     username:{
@@ -43,5 +45,46 @@ const userSchema=new mongoose.Schema({
     },
 
 },{timestamps:true});
+
+
+//bcrypt
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password"))return next();
+
+        this.password=bcrypt.hash(this.password,10)
+        next()
+    
+    
+})
+
+//custom method
+//to check is password correct or not 
+userSchema.methods.isPasswordCorrect= async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname:this.fullname,
+        },
+        process.env.ACCESS_TOKEN_SECRET, // Make sure to set this environment variable
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+    );
+}
+   
+userSchema.methods.generateRefreshToken=function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET, // Make sure to set this environment variable
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+    );
+}
 
 export const User=mongoose.model("User",userSchema);
