5140a18cfeb77f4cdc383d0c483e111c560c9573
Saurabh8840 <saurabhofficial8840@gmail.com>
2025-08-06 23:53:46 +0530

    updates controller

--- lect-6_backend/src/middlewares/auth.middleware.js
+++ lect-6_backend/src/middlewares/auth.middleware.js
@@ -1,5 +1,7 @@
 //it will verify is user exist or not
 
+
+
 import { User } from "../models/user.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -30,3 +32,47 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     throw new ApiError(401,error?.message||"Invalid access token")
   }
 });
+
+
+
+
+
+// import { ApiError } from "../utils/ApiError";
+// import { asyncHandler } from "../utils/asyncHandler";
+// import {User} from "../models/user.models";
+// import jwt from "jsonwebtoken";
+
+// export const verifyJWT=asyncHandler(async(req,res,async next=>{
+      
+
+//       try {
+//         //token ka access kaise lenge 
+//         //cookie pareser se 
+//         //yha pe yaa to cookies se yaa header me hooo req.header se mila aur phir js ke replace method se 
+//         // jha bearer aur space ho usko empty krr do "" bss kaam hoo gya 
+//         const token =req.cookies?.accessToken ||req.header("Authorization")?.replace("Bearer ","")
+//         //ya cookie se ya header se cookie ka acces aa gya nhi hai to error 
+//         if(!token){
+//           throw new ApiError(401,"Unauthoried request")
+//         }
+        
+//         //token ke ander kafi info hoti hai usko decode krne ke liye jwt chaihiye
+//         //import jwt
+//         const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+  
+//         //add verify ho gya
+  
+//         const user =await User.findById(decodedToken?._id).select("-password -refreshToken")
+  
+//         if(!user){
+//           //discuss about frontend 
+//           throw new ApiError(401,"invalid access token")
+//         }
+  
+//         req.user=user;
+//         next()
+//       } catch (error) {
+//            throw new ApiError(401,error?.message||"Invalid access token")
+//       }
+
+//    }))
