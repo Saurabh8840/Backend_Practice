b1e62b1839c0006a88405fadb5c74e501ccede2b
Saurabh8840 <saurabhofficial8840@gmail.com>
2025-08-12 23:37:53 +0530

    created routes

---
+++ RestfulApi/backend/src/routes/user.route.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { getUser } from "../controllers/users.controllers.js";
+import { newUser } from "../controllers/users.controllers.js";
+import { updateUser } from "../controllers/users.controllers.js";
+import { deleteUser } from "../controllers/users.controllers.js";
+
+
+const router=Router();
+
+router.route("/getusers").get(getUser);
+router.route("/newuser").post(newUser)
+router.route("/updateUser/:id").put(updateUser)
+router.route("/delete/:id").delete(deleteUser)
+export default router;
+
+
+
+// import { Router } from "express";
+
+// const router=Router();
+
+// router.route('/register').post();
+
+// export default router;
