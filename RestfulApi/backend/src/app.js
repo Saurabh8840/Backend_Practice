c57d32e763f33c028e0d480982871a0e11e308a2
Saurabh8840 <saurabhofficial8840@gmail.com>
2025-08-15 23:39:46 +0530

    restfule api app updated

--- RestfulApi/backend/src/app.js
+++ RestfulApi/backend/src/app.js
@@ -15,11 +15,3 @@ app.use("/api/users", userRouter)
 
 export {app}
 
-// import express from "express"
-// import userRouter from "./routes/user.route.js";
-
-// const app=express();
-
-// app.use(express.json())
-
-// app.use("/api/user",userRouter)
