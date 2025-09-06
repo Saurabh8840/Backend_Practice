6f700c40b0a6973b739b6da8615f3a3f6121b162
Saurabh8840 <saurabhofficial8840@gmail.com>
2025-09-07 01:03:39 +0530

    Added user model and CRUD

--- PrismaProject/src/index.ts
+++ PrismaProject/src/index.ts
@@ -67,63 +67,141 @@ import {PrismaClient} from "@prisma/client"
 const prisma=new PrismaClient();
 
 
-async function insertUser(username:string,password:string,firstname:string,lastname:string){
+// async function insertUser(username:string,password:string,firstname:string,lastname:string){
 
-    const res=await prisma.user.create({
-          data:{
-              username,
-              password,
-              firstname,
-              lastname
-          }
-    })
-    console.log("Inserted ",res);
+//     const res=await prisma.user.create({
+//           data:{
+//               username,
+//               password,
+//               firstname,
+//               lastname
+//           }
+//     })
+//     console.log("Inserted ",res);
 
-}
+// }
+
+// async function getUser(username:string) {
+//     const res=await prisma.user.findFirst({
+//         where:{
+//             username,
+//         }
+//     })
+//     console.log("Fetched",res);
+// }
+
+// async function updateUser(username:string,firstname:string,lastname:string){
+//     const res=await prisma.user.update({
+//         where:{
+//             username
+//         },
+//         data:{
+//             firstname,lastname
+//         }
+//     })
+//     console.log("updated",res);
+// }
+
+
+// async function getAllUser() {
+//     const users=await prisma.user.findMany();
+//     console.log("All users;",users);
+// } 
+
+// async function deleteUser(username: string) {
+//     const deleteUser = await prisma.user.delete({
+//         where: { username }
+//     });
+//     console.log("Deleted user:", deleteUser);
+// }
+//----------------------------------------------------------
+
+async function InsertUser(username:string,password:string,firstname:string,lastname:string) {
+   
+
+    try {
+        const res =await prisma.userji.create({
+            data:{
+                username,
+                password,
+                firstname,
+                lastname
+            }
+        })
+        console.log("inserted data",res)
+    } catch (error) {
+        console.error("error while inserting",error);
+    }
+    
+} 
 
 async function getUser(username:string) {
-    const res=await prisma.user.findFirst({
-        where:{
-            username,
-        }
-    })
-    console.log("Fetched",res);
+    
+    try {
+        const res=await prisma.userji.findUnique({
+            where:{
+                username
+            }
+        })
+        console.log("user detail",res);
+    } catch (error) {
+        console.log("erro while getuser",error);
+    }
 }
 
+
+//update user
 async function updateUser(username:string,firstname:string,lastname:string){
-    const res=await prisma.user.update({
+
+    const res=await prisma.userji.update({
         where:{
             username
         },
         data:{
-            firstname,lastname
+            firstname,
+            lastname
         }
     })
-    console.log("updated",res);
 }
 
-
-async function getAllUser() {
-    const users=await prisma.user.findMany();
-    console.log("All users;",users);
+//getuser
+async function getAllUser(){
+    const res= await prisma.userji.findMany();
+    console.log("all detail :",res);
 }
 
-async function deleteUser(username: string) {
-    const deleteUser = await prisma.user.delete({
-        where: { username }
-    });
-    console.log("Deleted user:", deleteUser);
-}
+//delete user
+async function deleteUser(username:string) {
+    const res=await prisma.userji.delete({
+        where:{
+            username
+        }
+    })
 
+}
 
 async function  main() {
     // await insertUser("saurabh1","123456","saurabh","tripathi");
     // await updateUser("saurabh1","saurabhji","tripathiji");
-   await  getUser("saurabh1");
-  
-   await deleteUser("admin1");
-    await getAllUser();
-    
+//     await  getUser("saurabh1");
+//     await deleteUser("admin1");
+//     await getAllUser(); 
+       //insert single data in table 
+       await InsertUser("smriti1","123456","smritiji","tripathi");
+       //review data 
+       await getUser("smriti1");
+       //insert few data 
+       await InsertUser("akriti1","123456","akritiji","tripathi");
+       await InsertUser("saurabh1","123456","saurabhiji","tripathi");
+       await InsertUser("sakshi1","123456","sakshiji","tripathi");
+       //update data
+       await updateUser("sakshi1","sakshiBahini","paglu");
+       // get all data
+       await getAllUser();
+       //delete
+       await deleteUser("sakshi1");
+       //get all data
+       await getAllUser();
 }
 
 main()
